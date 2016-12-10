a55b2bdd73f8d43941aa10d1e8173d9f0a7b88ab
Robert Basic <robertbasic.com@gmail.com>
2016-12-10 10:01:32 +0100

    The piggy bank doesn't deposit correctly

--- test/PiggyBankTest/Application/PiggyBankTest.php
+++ test/PiggyBankTest/Application/PiggyBankTest.php
@@ -22,4 +22,21 @@ class PiggyBankTest extends \PHPUnit_Framework_TestCase
 
         self::assertSame($expected, $result);
     }
+
+    public function test_total_deposit_amount_is_correct_after_two_deposits()
+    {
+        $firstAmount = '100';
+        $secondAmount = '55.6';
+
+        $piggyBank = new PiggyBank();
+
+        $piggyBank->deposit($firstAmount);
+        $piggyBank->deposit($secondAmount);
+
+        $result = $piggyBank->getTotalDeposit();
+
+        $expected = 155.6;
+
+        self::assertSame($expected, $result);
+    }
 }
